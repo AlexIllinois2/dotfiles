19a57e3945c0741e904d216e5c9330eb2bbb6cd6
v <AlexIllinois@163.com>
2026-06-26 11:20:22 +0800

    fix

--- .local/shell/env/path.sh
+++ .local/shell/env/path.sh
@@ -4,4 +4,6 @@ export APP_IMAGE_PATH=${HOME}/.local/appimage
 export _LD_LIBRARY_PATH=${HOME}/.local/lib
 
 export PATH=$SCRIPT_PATH:$BIN_PATH:$APP_IMAGE_PATH:$_LD_LIBRARY_PATH:/usr/local/bin:$PATH
-export LD_LIBRARY_PATH=${_LD_LIBRARY_PATH}:$LD_LIBRARY_PATH
+if [[ "$(uname -o)" != "Android" ]]; then
+  export LD_LIBRARY_PATH=${_LD_LIBRARY_PATH}:$LD_LIBRARY_PATH
+fi
