5e3920e229b9b00797225172a33513e79d28a997
v <AlexIllinois@163.com>
2026-09-06 11:25:04 +0800

    nvm

--- .local/shell/rc/nvm.sh
+++ .local/shell/rc/nvm.sh
@@ -1,3 +1,6 @@
 
 export NVM_DIR="$HOME/.local/app/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+export NVM_NODEJS_ORG_MIRROR=https://npmmirror.com/mirrors/node
+export NVM_IOJS_ORG_MIRROR=https://npmmirror.com/mirrors/iojs
